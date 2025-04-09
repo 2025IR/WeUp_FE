31d02869b7c8d9da93c52128a305af736c92c5e2
정윤석 <mar0722@naver.com>
2025-04-09 16:43:47 +0900

    Style: GlobalStyle 초기 설정 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from "@emotion/react";
 import theme from "./styles/theme";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <div>Hello, WeUp 👋</div>
     </ThemeProvider>
   );
