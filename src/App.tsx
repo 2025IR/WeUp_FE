c78c2ae1ee1b8d79acdc749de3c5418d1cae583d
정윤석 <mar0722@naver.com>
2025-04-21 22:15:51 +0900

    refactor: 글로벌 스타일 테마 연동 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from "@emotion/react";
-import theme from "./styles/theme";
+import { lightTheme } from "./styles/theme";
 import GlobalStyle from "./styles/GlobalStyle";
 import { useRoutes } from "react-router-dom";
 import routes from "./routes/routes";
 
 function App() {
   const element = useRoutes(routes);
+  const theme = lightTheme;
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
