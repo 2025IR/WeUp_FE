267fd61a40c3896ceaae880ab50d170d50903dac
정윤석 <mar0722@naver.com>
2025-04-28 02:08:05 +0900

    style: Header 컴포넌트 스타일링

--- src/components/layout/AppLayout/Header/index.tsx
+++ src/components/layout/AppLayout/Header/index.tsx
@@ -1,31 +1,34 @@
 import logo_light from "@/assets/logo/logo_light.svg";
 import Button from "@/components/common/Button";
 import { AiOutlineBell } from "react-icons/ai";
+import { Container, Content, Logo, UserInfo, UserSection } from "./style";
 // import logo_dark from "@/assets/logo/logo_light.svg";
 
 const Header = () => {
   return (
-    <div>
-      <div>
-        <img src={logo_light} alt="logo" />
-        <p>we:up</p>
-      </div>
+    <Container>
+      <Content>
+        <Logo>
+          <img src={logo_light} alt="logo" />
+          <p>we:up</p>
+        </Logo>
 
-      <div>
-        <div>
-          <Button>yundoll</Button>
-          <Button variant="secondary">mar0722@naver.com</Button>
-          <Button variant="secondary" iconOnly>
-            <AiOutlineBell />
-          </Button>
-        </div>
+        <UserSection>
+          <UserInfo>
+            <Button>yundoll</Button>
+            <Button variant="secondary">mar0722@naver.com</Button>
+            <Button variant="secondary" iconOnly>
+              <AiOutlineBell />
+            </Button>
+          </UserInfo>
 
-        <img
-          src="https://pixabay.com/get/ga883c5369a2e4fb6ee4343c04972f15fec8b0a3357f26bb30ce697ca4ce2e6262db674fd27bbe286433358fad1c22d041d18d2fd32d0869e065edbdf4d703b8ee8809a17e0e28dac7387f9b4030c57ed_640.png"
-          alt="profile-iamge"
-        />
-      </div>
-    </div>
+          <img
+            src="https://pixabay.com/get/ga883c5369a2e4fb6ee4343c04972f15fec8b0a3357f26bb30ce697ca4ce2e6262db674fd27bbe286433358fad1c22d041d18d2fd32d0869e065edbdf4d703b8ee8809a17e0e28dac7387f9b4030c57ed_640.png"
+            alt="profile-iamge"
+          />
+        </UserSection>
+      </Content>
+    </Container>
   );
 };
 
