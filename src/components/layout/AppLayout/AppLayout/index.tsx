6117d5dd991e7d1cc51b7e028de7272a73044718
정윤석 <mar0722@naver.com>
2025-04-28 22:42:12 +0900

    fix: 라우터 구조 수정 및 관련 레이아웃 컴포넌트 추가

--- src/components/layout/AppLayout/AppLayout/index.tsx
+++ src/components/layout/AppLayout/AppLayout/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import SideNav from "./navigation/SideNav";
+import Header from "../Header";
+import SideNav from "../navigation/SideNav";
 
 const AppLayout = () => {
   return (
