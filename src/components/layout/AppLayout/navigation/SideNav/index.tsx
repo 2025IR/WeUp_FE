56a6040f43380bfe853ff00a9a0557bad2fdb273
정윤석 <mar0722@naver.com>
2025-04-28 13:26:10 +0900

    feat: 프로젝트 리스트 분리 및 컴포넌트 구현

--- src/components/layout/AppLayout/navigation/SideNav/index.tsx
+++ src/components/layout/AppLayout/navigation/SideNav/index.tsx
@@ -10,6 +10,7 @@ import {
   NavWrapper,
   ProjectWrapper,
 } from "./style";
+import ProjectsList from "../ProjectsList";
 
 const SideNav = () => {
   return (
@@ -26,30 +27,7 @@ const SideNav = () => {
       <ProjectWrapper>
         {/* 프로젝트 리스트 */}
         <NavTitle>Projects</NavTitle>
-        <NavItem>
-          <IconLabel
-            type="image"
-            icon="https://pixabay.com/get/gdb9dac014b96dd4536b66321cf1f133c9fe1be67c2b812743fae17b8fe1c5886e8ce622f72a0ca9e49f8b88695b0c1e6413aa0cbfbc4209ef3d0a0302b5907f70d25e4fe8d5a05c3cf6ed28514bd8730_640.jpg"
-          >
-            we:up
-          </IconLabel>
-        </NavItem>
-        <NavItem>
-          <IconLabel
-            type="image"
-            icon="https://pixabay.com/get/gdb9dac014b96dd4536b66321cf1f133c9fe1be67c2b812743fae17b8fe1c5886e8ce622f72a0ca9e49f8b88695b0c1e6413aa0cbfbc4209ef3d0a0302b5907f70d25e4fe8d5a05c3cf6ed28514bd8730_640.jpg"
-          >
-            Quiz? Up!
-          </IconLabel>
-        </NavItem>
-        <NavItem>
-          <IconLabel
-            type="image"
-            icon="https://pixabay.com/get/gdb9dac014b96dd4536b66321cf1f133c9fe1be67c2b812743fae17b8fe1c5886e8ce622f72a0ca9e49f8b88695b0c1e6413aa0cbfbc4209ef3d0a0302b5907f70d25e4fe8d5a05c3cf6ed28514bd8730_640.jpg"
-          >
-            Rumon
-          </IconLabel>
-        </NavItem>
+        <ProjectsList />
       </ProjectWrapper>
       <NavWrapper>
         <IconLabel icon={<AiFillSun />}>toggle</IconLabel>
