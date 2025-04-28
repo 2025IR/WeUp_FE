7b50b1bd49294b0bf6a885a0779a9c0a55053b49
정윤석 <mar0722@naver.com>
2025-04-28 19:47:34 +0900

    feat: ThemeToggle 컴포넌트 구현

--- src/components/layout/AppLayout/navigation/ThemeToggle/index.tsx
+++ src/components/layout/AppLayout/navigation/ThemeToggle/index.tsx
@@ -1,3 +1,19 @@
-const ThemeToggle = () => {};
+import { useState } from "react";
+
+const ThemeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleHandler = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <>
+      <div onClick={toggleHandler}>
+        <div />
+      </div>
+    </>
+  );
+};
 
 export default ThemeToggle;
