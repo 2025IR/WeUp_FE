adcd5a33af78fffaa7c6b26daa20598830cd5064
정윤석 <mar0722@naver.com>
2025-04-29 18:07:42 +0900

    remove: 로그인 페이지 테스트 코드 삭제

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,5 @@
-import Input from "@/components/common/Input";
-
 const Login = () => {
-  return (
-    <div>
-      <Input type="email" label="Email Address" isButton="인증" />
-      <Input type="email" label="Email Address" isButton="확인" />
-      <Input type="email" label="Email Address" isButton="인증" />
-      <Input type="email" label="Email Address" />
-    </div>
-  );
+  return <div>로그인</div>;
 };
 
 export default Login;
