d926c222f29ccc8883d3eceaf121fe0eeb976bb6
정윤석 <mar0722@naver.com>
2025-05-02 23:44:16 +0900

    feat: 프로젝트 생성 모달 내부 UI 구현 및 스타일링

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,10 +1,12 @@
 import ProjectCard from "@/components/projects/ProjectCard";
-import { Container, Header, Main } from "./style";
+import { Container, Header, Main, ModalContainer, UploadButton } from "./style";
 import Button from "@/components/common/Button";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Modal from "@/components/common/Modal";
-import { AiFillFolderAdd } from "react-icons/ai";
+import { AiFillFolderAdd, AiOutlineUpload } from "react-icons/ai";
+import Input from "@/components/common/Input";
+import IconLabel from "@/components/common/IconLabel";
 
 const mockProjects = [
   {
@@ -65,7 +67,24 @@ const Projects = () => {
           icon={<AiFillFolderAdd />}
           title="New Project"
         >
-          <div></div>
+          <ModalContainer>
+            <img
+              src="https://pixabay.com/get/gd18b5061c19770af1bbd98ff5f270e14287f99abedbbb2f40f7da254c6ffffede7381e165b8c0c5a31ff8b13f62fb279b815dcaed0017039c8939cb3d680f9ee73f2c2dc54073dc1fd4657769d3a74fb_640.jpg"
+              alt="project profile priview"
+            />
+            <div>
+              <Input label="Project Name:" />
+              <UploadButton>
+                <IconLabel
+                  icon={<AiOutlineUpload />}
+                  gap="0.375rem"
+                  colors="textLight"
+                >
+                  Upload Image
+                </IconLabel>
+              </UploadButton>
+            </div>
+          </ModalContainer>
         </Modal>
       )}
     </Container>
