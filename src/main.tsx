372da27c2dbc974903c773b09bd0aa25296cf8a3
정윤석 <mar0722@naver.com>
2025-05-05 19:53:43 +0900

    chore: queryClient 활성화

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,19 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/index.ts";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./query/reactQueryClient.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
