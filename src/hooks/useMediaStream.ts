3d8c0e4ca2099818a5cc4a7c73e2f171c438737f
정윤석 <mar0722@naver.com>
2025-05-10 11:38:32 +0900

    feat: 미디어 스트림 연결 훅 구현

---
+++ src/hooks/useMediaStream.ts
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from "react"
+
+export const useMediaStream = (audioOn = true, videoOn = true) => {
+    const [stream, setStream] = useState<MediaStream | null>(null);
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+
+    const getStream = async (audio: boolean, video: boolean) => {
+        try {
+            const newStream = await navigator.mediaDevices.getUserMedia({
+                audio, video
+            });
+            
+            setStream(newStream);
+            if (videoRef.current) {
+                videoRef.current.srcObject = newStream;
+            }
+        } catch (e) {
+            console.error("미디어 권한 요청 실패:", e);
+        }
+    };
+
+    const stopStream = () => {
+        stream?.getTracks().forEach(track=> track.stop());
+        setStream(null);
+    };
+
+    useEffect(()=>{
+        getStream(audioOn, videoOn);
+        return stopStream;
+    }, [audioOn, videoOn]);
+
+    return {stream, videoRef, stopStream};
+}
