40052ee02a6a5f15545e5a6fef6361a376c9e439
정윤석 <mar0722@naver.com>
2025-05-10 15:57:26 +0900

    fix: 응답 데이터 res.data => res.data.data로 수정

--- src/apis/auth/auth.ts
+++ src/apis/auth/auth.ts
@@ -3,5 +3,5 @@ import publicInstance from "../publicInstance";
 
 export const login = async (data: LoginRequest): Promise<LoginResponse> => {
   const res = await publicInstance.post("/user/signin", data);
-  return res.data;
+  return res.data.data;
 };
