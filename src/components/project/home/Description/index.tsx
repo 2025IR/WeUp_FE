3b84b9ff98127d434f8cf96dd45f73cc8358df8f
정윤석 <mar0722@naver.com>
2025-05-11 17:34:49 +0900

    feat: 프로젝트 Home 페이지 UI 구현 및 스타일링

--- src/components/project/home/Description/index.tsx
+++ src/components/project/home/Description/index.tsx
@@ -1,23 +1,30 @@
 import { useProjectInfo } from "@/query/project/useProjectInfo";
+import { useState } from "react";
 import { BiEditAlt } from "react-icons/bi";
 import { useParams } from "react-router-dom";
+import { StyledButton, StyledTextarea, Wrapper } from "./style";
 
 const Description = () => {
   const { projectId } = useParams();
   const project_id = Number(projectId);
 
   const { data, isLoading, error } = useProjectInfo(project_id);
+  const [isEditable, setIsEditable] = useState(false);
 
   if (isLoading) return <p>로딩 중...</p>;
   if (error) return <p>에러 발생</p>;
 
   return (
-    <div>
-      <textarea name="" id="">
-        {data.description}
-      </textarea>
-      <BiEditAlt />
-    </div>
+    <Wrapper>
+      <StyledTextarea
+        value={data.description}
+        readOnly={!isEditable}
+        rows={3}
+      />
+      <StyledButton>
+        <BiEditAlt />
+      </StyledButton>
+    </Wrapper>
   );
 };
 
