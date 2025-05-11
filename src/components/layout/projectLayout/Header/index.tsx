e0ac8f4de350ed3c5f7423e933d727a8d4980d2f
정윤석 <mar0722@naver.com>
2025-05-11 17:51:41 +0900

    feat: 선택된 프로젝트 정보 store에 전역 상태관리

--- src/components/layout/projectLayout/Header/index.tsx
+++ src/components/layout/projectLayout/Header/index.tsx
@@ -3,21 +3,17 @@ import Label from "@/components/common/Label";
 import { IoMdSettings } from "react-icons/io";
 import { Container, InfoSection, LabelSection, TabSection } from "./style";
 import ProjectNav from "../Navigation";
-
-export const mockProject = {
-  projectName: "Rumon",
-  projectImage:
-    "https://we-up-public.s3.ap-northeast-2.amazonaws.com/smiley1.png",
-  description: "생성형 AI를 이용한 게이미케이션 투두 앱",
-};
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 
 const ProjectHeader = () => {
+  const { name, image } = useSelector((state: RootState) => state.project);
   return (
     <Container>
-      <img src={mockProject.projectImage} alt="project image" />
+      <img src={image} alt="project image" />
       <TabSection>
         <InfoSection>
-          <h1>{mockProject.projectName}</h1>
+          <h1>{name}</h1>
           <LabelSection>
             <Label>진행중</Label>
             <Label colors="secondary">
