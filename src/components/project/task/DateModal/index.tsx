2a2f242c4a71f531b9345c93342025506f22902e
정윤석 <mar0722@naver.com>
2025-06-02 01:40:20 +0900

    style: react-day-picker 커스텀 스타일링

--- src/components/project/task/DateModal/index.tsx
+++ src/components/project/task/DateModal/index.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import { DateRange, DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { CalendarWrapper } from "./style";
+
 const DateModal = () => {
-  return <div>달력</div>;
+  const [range, setRange] = useState<DateRange | undefined>(undefined);
+
+  return (
+    <CalendarWrapper>
+      <DayPicker
+        mode="range"
+        selected={range}
+        onSelect={setRange}
+        numberOfMonths={1}
+      />
+    </CalendarWrapper>
+  );
 };
 
 export default DateModal;
