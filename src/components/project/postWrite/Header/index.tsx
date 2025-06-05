33fd0c94e2a4d1c3e950bcdb4886d80a11616a30
정윤석 <mar0722@naver.com>
2025-06-05 16:20:42 +0900

    feat: 작성 후 라우팅 및 렌더링 로직 추가

--- src/components/project/postWrite/Header/index.tsx
+++ src/components/project/postWrite/Header/index.tsx
@@ -1,20 +1,28 @@
 import Button from "@/components/common/Button";
 import { BiArrowBack } from "react-icons/bi";
 import { ButtonWrapper, HeaderWrapper, LeftSection } from "./style";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   onSubmit: () => void;
 };
 
 const PostWriteHeader = ({ onSubmit }: Props) => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
   return (
     <HeaderWrapper>
       <LeftSection>
-        <BiArrowBack />
+        <BiArrowBack onClick={handleBack} />
         <h1>게시글 작성</h1>
       </LeftSection>
       <ButtonWrapper>
-        <Button variant="secondary">취소</Button>
+        <Button variant="secondary" onClick={handleBack}>
+          취소
+        </Button>
         <Button onClick={onSubmit}>등록</Button>
       </ButtonWrapper>
     </HeaderWrapper>
--- src/query/board/useCreatePost.ts
+++ src/query/board/useCreatePost.ts
@@ -1,16 +1,29 @@
 import { createPost } from "@/apis/board/board";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 export const useCreatePost = () => {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: ({
+    mutationFn: async ({
       projectId,
       formData,
     }: {
       projectId: number;
       formData: FormData;
     }) => {
-      createPost(projectId, formData);
+      return await createPost(projectId, formData);
+    },
+
+    onSuccess: (res, { projectId }) => {
+      console.log("✅ 게시글 등록 성공", res);
+      queryClient.invalidateQueries({ queryKey: ["boardList", projectId] });
+      navigate(`/project/${projectId}/board`);
+    },
+
+    onError: (error) => {
+      console.error("❌ 게시글 등록 실패", error);
     },
   });
 };
