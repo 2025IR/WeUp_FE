71ad06c9e75fd7826fc210be777a5f177b6ac860
정윤석 <mar0722@naver.com>
2025-07-28 16:53:29 +0900

    refactor: ChatMessageCard 컴포넌트 memoization 적용

--- src/components/project/chat/ChatMessageCard/index.tsx
+++ src/components/project/chat/ChatMessageCard/index.tsx
@@ -12,53 +12,58 @@ import {
 import { formatChatTime } from "@/utils/formatTime";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
+import React from "react";
 
-const ChatMessageCard = ({
-  senderId,
-  senderName,
-  senderProfileImage,
-  message,
-  sentAt,
-  isImage,
-  isShowTime,
-  isShowUserInfo,
-}: ChatCardProps) => {
-  const userId = useSelector((state: RootState) => state.auth.userId);
-  const isRight = senderId === userId;
+const ChatMessageCard = React.memo(
+  ({
+    senderId,
+    senderName,
+    senderProfileImage,
+    message,
+    sentAt,
+    isImage,
+    isShowTime,
+    isShowUserInfo,
+  }: ChatCardProps) => {
+    const userId = useSelector((state: RootState) => state.auth.userId);
+    const isRight = senderId === userId;
 
-  if (isRight) {
+    if (isRight) {
+      return (
+        <MyCardContainer>
+          {isShowTime && <MessageTime>{formatChatTime(sentAt)}</MessageTime>}
+          {isImage ? (
+            <ImageCard src={message} alt="" />
+          ) : (
+            <MyTextCard>
+              <p>{message}</p>
+            </MyTextCard>
+          )}
+        </MyCardContainer>
+      );
+    }
+
     return (
-      <MyCardContainer>
+      <ContainerCard>
+        <ImgWrapper>
+          {isShowUserInfo && (
+            <img src={senderProfileImage} alt="user-profile" />
+          )}
+        </ImgWrapper>
+        <TextCardWrapper>
+          {isShowUserInfo && <p>{senderName}</p>}
+          {isImage ? (
+            <ImageCard src={message} />
+          ) : (
+            <TextCard>
+              <p>{message}</p>
+            </TextCard>
+          )}
+        </TextCardWrapper>
         {isShowTime && <MessageTime>{formatChatTime(sentAt)}</MessageTime>}
-        {isImage ? (
-          <ImageCard src={message} alt="" />
-        ) : (
-          <MyTextCard>
-            <p>{message}</p>
-          </MyTextCard>
-        )}
-      </MyCardContainer>
+      </ContainerCard>
     );
   }
-
-  return (
-    <ContainerCard>
-      <ImgWrapper>
-        {isShowUserInfo && <img src={senderProfileImage} alt="user-profile" />}
-      </ImgWrapper>
-      <TextCardWrapper>
-        {isShowUserInfo && <p>{senderName}</p>}
-        {isImage ? (
-          <ImageCard src={message} />
-        ) : (
-          <TextCard>
-            <p>{message}</p>
-          </TextCard>
-        )}
-      </TextCardWrapper>
-      {isShowTime && <MessageTime>{formatChatTime(sentAt)}</MessageTime>}
-    </ContainerCard>
-  );
-};
+);
 
 export default ChatMessageCard;
