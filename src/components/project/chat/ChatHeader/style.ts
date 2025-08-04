41ef15c4f72fe48a122c857dc474b85886427bbb
정윤석 <mar0722@naver.com>
2025-08-04 14:02:08 +0900

    feat: 채팅방 이름수정/초대/나가기 모달 및 기능 구현

--- src/components/project/chat/ChatHeader/style.ts
+++ src/components/project/chat/ChatHeader/style.ts
@@ -39,3 +39,64 @@ export const IconWrapper = styled.div`
     cursor: pointer;
   }
 `;
+
+export const MemberSection = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  > p {
+    padding: 0.25rem 0;
+    font-size: 10px;
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+    color: ${({ theme }) => theme.colors.textLight};
+  }
+`;
+
+export const MemberItemWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  border: 1px solid ${({ theme }) => theme.colors.border};
+  background-color: ${({ theme }) => theme.colors.background};
+  border-radius: ${({ theme }) => theme.radius.sm};
+  overflow: hidden;
+`;
+
+export const MemberItem = styled.div<{ isSelected: boolean }>`
+  width: 100%;
+  padding: 0.5rem 1rem;
+
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  cursor: pointer;
+  background-color: ${({ isSelected, theme }) =>
+    isSelected ? theme.colors.secondary : theme.colors.background};
+
+  > img {
+    width: 1.25rem;
+    height: 1.25rem;
+    border-radius: ${({ theme }) => theme.radius.full};
+  }
+
+  > p {
+    flex: 1;
+    font-size: ${({ theme }) => theme.fontSize.caption};
+    color: ${({ theme, isSelected }) =>
+      isSelected ? theme.colors.text : theme.colors.textLight};
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+  }
+
+  > svg {
+    width: 1.5rem;
+    height: 1.5rem;
+    color: ${({ theme, isSelected }) =>
+      isSelected ? theme.colors.primary : theme.colors.textLight};
+  }
+`;
