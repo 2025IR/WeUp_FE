67d494db62387fa5e822f9d057216e0eb8a2a57b
정윤석 <mar0722@naver.com>
2025-09-09 10:48:02 +0900

    refactor: 게시판 api 성공, 실패 여부 토스트 알림 출력

--- src/query/board/useUpdatePost.ts
+++ src/query/board/useUpdatePost.ts
@@ -1,7 +1,11 @@
 import { updatePost } from "@/apis/board/board";
+import { setApiMessage } from "@/store/alert";
 import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useDispatch } from "react-redux";
 
 export const useUpdatePost = () => {
+  const dispatch = useDispatch();
   return useMutation({
     mutationFn: ({
       postId,
@@ -10,5 +14,16 @@ export const useUpdatePost = () => {
       postId: number;
       formData: FormData;
     }) => updatePost(postId, formData),
+    onSuccess: () => {
+      dispatch(setApiMessage({ message: "게시글 수정 성공", type: "success" }));
+    },
+    onError: (err: AxiosError<{ message: string }>) => {
+      dispatch(
+        setApiMessage({
+          message: err.response?.data.message ?? "게시글 등록 실패",
+          type: "error",
+        })
+      );
+    },
   });
 };
